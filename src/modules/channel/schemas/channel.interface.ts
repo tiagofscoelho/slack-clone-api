d396da082cea5770ea84224f8dbbb4a71aa00063
Tiago Coelho <tiagofscoelho@gmail.com>
2018-07-08 16:12:06 +0100

    Fix typo

--- src/modules/channel/schemas/channel.interface.ts
+++ src/modules/channel/schemas/channel.interface.ts
@@ -4,5 +4,5 @@ export class ChannelInterface {
   readonly name: string
   readonly private: boolean
   readonly purpose: string
-  readonly createdB: UserInterface
+  readonly createdBy: UserInterface
 }
